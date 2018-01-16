3cae72d0b2820c3cbee99328cabc2d542683d9e7
dionCavanaugh <dioncavanaugh@hotmail.com>
2018-01-16 05:59:06 -0500

    completed server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var mysql = require('mysql');
 var fs = require('fs');
 var csv = require('fast-csv');
 
+//now create an event emitter for our stream we will be reading from
+var stream = fs.createReadStream('top10.csv');
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3000,
@@ -9,3 +12,33 @@ var connection = mysql.createConnection({
     password: "root",
     database: "top_songsDB"
 });
+
+connection.connect(function(err){
+    if (err) throw (err)
+    console.log("Connected to db");
+    writeFile();//function that will parse csv file and save to db
+});
+
+//define function that will read csv file and insert it into db
+function writeFile() {
+
+    csv
+        .fromStream(stream, {headers: ["position", "artist", "song", "year", "raw_total", "raw_usa", "raw_uk", "raw_eur", "raw_row"]})
+        .on('data', function(data){
+            connection.query(
+                "INSERT INTO top10 (position, artist, song, year, raw_total, raw_usa, raw_uk, raw_eur, raw_row)" +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            [data.position, data.artist, data.song, data.year, data.raw_total, data.raw_usa, data.raw_uk, data.raw_eur,
+            data.raw_row],
+            function(err){
+                if (err) throw (err)
+                console.log("Saved to db!");
+            }
+            );
+        })
+        .on('end', function(data){
+            console.log('done!');
+        });
+
+
+}
